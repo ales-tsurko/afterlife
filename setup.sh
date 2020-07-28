3578622944c581c0be950f61aec85c94a5525a05
Ales Tsurko <ales.tsurko@gmail.com>
2020-07-29 00:55:05 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -112,7 +112,7 @@ sudo snap install telegram-desktop opera
 # Alacritty - terminal emulator
 bash -c "$(wget -qO- https://gist.github.com/ales-tsurko/cc8cb59f6d5a1aa95512e81e3dfe64ff/raw/524559dd1557de73f6e33ed0612fe747d89d633f/install-alacritty-ubuntu.sh)"
 rm -f ~/.alacritty.yaml
-mv config/alacritty/.alacritty.yaml ~/
+mv config/alacritty/.alacritty.yml ~/
 
 
 # Themes
